79def75ca9260346ce9dab7984867e33ad73a0fe
SSep <ssep4u@gmail.com>
2020-02-11 10:53:17 +0900

    config/urls bookmark/urls

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path('bookmark/', include('bookmark.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
