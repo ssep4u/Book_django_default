90931c152b90c3cfc380e5294982300ace46fac2
SSep <ssep4u@gmail.com>
2020-02-11 10:54:12 +0900

    bookmark/views BookmarkListView(ListView)

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Bookmark
 
-# Create your views here.
+
+class BookmarkListView(ListView):
+    model = Bookmark
